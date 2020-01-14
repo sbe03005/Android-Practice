b80abad31306eb76c8d1041dbe7758481153d2a3
parkwoongki <sbe03005@naver.com>
2020-01-14 16:20:55 +0900

    Simple EditText Example

---
+++ EditTextTest/app/src/main/java/cse/mobile/edittexttest/MainActivity.java
@@ -0,0 +1,31 @@
+package cse.mobile.edittexttest;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText editTextName = findViewById(R.id.editTextName);
+        editTextName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    Toast.makeText(MainActivity.this, "Message Sent : "+ editTextName.getText(), Toast.LENGTH_SHORT).show();
+                }
+                return handled;
+            }
+        });
+    }
+}
